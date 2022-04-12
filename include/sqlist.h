a7ad169666e4147cf5e7b514c97acaed9eb40cf8
lh <2226958871@qq.com>
2022-04-13 00:06:52 +0800

    first commit

--- include/sqlist.h
+++ include/sqlist.h
@@ -4,7 +4,10 @@
 
 #include "common.h"
 
-// 静态分配
+#include <stdio.h>
+#include <string.h>
+
+// 静态分配 default
 #define MaxSize 50  // 定义线性表的最大长度
 typedef struct {
     int data[MaxSize];  // 顺序表的长度
@@ -20,10 +23,40 @@ typedef struct {
  
 /**
  * @brief 在顺序表的第i(1<=i<=L.length+1)个位置插入新元素e
+ *        时间复杂度：O(n)
  * 
+ * @param L 
+ * @param i 
+ * @param e 
+ * @return true 
+ * @return false 
  */
 bool ListInsert(SqList &L, int i, int e);  // 插入操作
+
+/**
+ * @brief 在顺序表的第i(1<=i<=L.length+1)个位置删除元素
+ *        时间复杂度：O(n)
+ * @param L 
+ * @param i 
+ * @param e 
+ * @return true 
+ * @return false 
+ */
 bool ListDelete(SqList &L, int i, int e);  // 删除操作
+
+/**
+ * @brief 查找顺序表中第一个元素值等于e的元素
+ * 
+ * @param L 
+ * @param e 
+ * @return int 
+ */
 int LocateElem(SqList L, int e);  // 查找操作
+
+SqList InitSqList();  // 初始化顺序表
+inline int Length(SqList &L);  // 求表长
+void PrintList(SqList &L); // 输出操作
+inline bool Empty(SqList &L);  // 判断是否为空
+void DestroyList(SqList &L); // 销毁操作
 
 #endif // !__SQLIST_H__
--- src/sqlist.cc
+++ src/sqlist.cc
@@ -12,12 +12,18 @@
 #include "../include/sqlist.h"
 
 bool ListInsert(SqList &L, int i, int e) {
+    /**
+     * @brief i < 1表示第一个元素是1 L.length + 1表示插入的最后一个元素是length+1
+     * 此处表示的是位序
+     * 
+     */
     if (i < 1 || i > L.length + 1) {  // 检查i的范围是否有效
         return false;
     }
     if (L.length >= MaxSize) {  // 检查存储空间是否已满
         return false;  
     }
+    // 此处表示的是下标
     for (int j = L.length; j >= i; j --) {
         L.data[j] = L.data[j - 1];  // 将第i个元素及之后的元素后移
     }
@@ -27,9 +33,48 @@ bool ListInsert(SqList &L, int i, int e) {
 }
 
 bool ListDelete(SeqList &L, int i, int e) {
-
+    if (i < 1 || i > L.length + 1) {
+        return false;
+    }
+    e = L.data[i - 1];
+    for (int j = i; j < L.length; j ++) {
+        L.data[j - 1] = L.data[j];
+    }
+    L.length --;
+    return true;
 } 
 
 int LocateElem(SqList L, int e) {
+    int i;
+    for (int i = 0; i < L.length; i ++) {
+        if (L.data[i] == e) {
+            return i + 1;  // 返回其位序
+        }
+    }
+    return 0;
+}
+
+SqList InitSqList() {
+    SqList sq;
+    sq.length = 0;
+    return sq;
+}
+
+int Length(SqList &L) {
+    return L.length;
+}
+
+void PrintList(SqList &L) {
+    for (int i = 0; i < L.length; i ++) {
+        DBG(GREEN"%d " NONE, L.data[i]);
+    }
+    DBG(GREEN"\n" NONE);
+}
+
+bool Empty(SqList &L) {
+    return L.length == 0;
+}
+
+void DestroyList(SqList &L) {
     
 }
